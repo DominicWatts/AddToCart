2ef204fb0787acc52457dd4651f2ba030c3f2e45
Dominic Watts <dominic.watts@xigen.co.uk>
2021-09-10 10:13:34 +0100

    1.0.0

---
+++ Helper/AddToCart.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * Copyright ©  All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Xigen\AddToCart\Helper;
+
+use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Framework\App\ActionInterface;
+use Magento\Catalog\Model\Product;
+use Magento\Framework\Url\Helper\Data;
+use Magento\Checkout\Helper\Cart;
+use Magento\Framework\App\Helper\Context;
+
+class AddToCart extends AbstractHelper
+{
+    /**
+     * @var Data
+     */
+    protected $urlHelper;
+
+    /**
+     * @var \Magento\Checkout\Helper\Cart
+     */
+    protected $_cartHelper;
+
+    /**
+     * @param \Magento\Framework\App\Helper\Context $context
+     * @param \Magento\Framework\Url\Helper\Data $urlHelper
+     * @param \Magento\Checkout\Helper\Cart $_cartHelper
+     */
+    public function __construct(
+        Context $context,
+        Data $urlHelper,
+        Cart $_cartHelper
+    ) {
+        $this->urlHelper = $urlHelper;
+        $this->_cartHelper = $_cartHelper;
+        parent::__construct($context);
+    }
+
+    /**
+     * Get post parameters
+     *
+     * @param Product $product
+     * @return array
+     */
+    public function getAddToCartPostParams(Product $product)
+    {
+        $url = $this->getAddToCartUrl($product, ['_escape' => false]);
+        return [
+            'action' => $url,
+            'data' => [
+                'product' => (int) $product->getEntityId(),
+                ActionInterface::PARAM_NAME_URL_ENCODED => $this->urlHelper->getEncodedUrl($url),
+            ]
+        ];
+    }
+
+    /**
+     * Retrieve url for add product to cart
+     *
+     * Will return product view page URL if product has required options
+     *
+     * @param \Magento\Catalog\Model\Product $product
+     * @param array $additional
+     * @return string
+     */
+    public function getAddToCartUrl($product, $additional = [])
+    {
+        if (!$product->getTypeInstance()->isPossibleBuyFromList($product)) {
+            if (!isset($additional['_escape'])) {
+                $additional['_escape'] = true;
+            }
+            if (!isset($additional['_query'])) {
+                $additional['_query'] = [];
+            }
+            $additional['_query']['options'] = 'cart';
+
+            return $this->getProductUrl($product, $additional);
+        }
+        return $this->_cartHelper->getAddUrl($product, $additional);
+    }
+
+    /**
+     * Retrieve Product URL using UrlDataObject
+     *
+     * @param \Magento\Catalog\Model\Product $product
+     * @param array $additional the route params
+     * @return string
+     */
+    public function getProductUrl($product, $additional = [])
+    {
+        if ($this->hasProductUrl($product)) {
+            if (!isset($additional['_escape'])) {
+                $additional['_escape'] = true;
+            }
+            return $product->getUrlModel()->getUrl($product, $additional);
+        }
+
+        return '#';
+    }
+
+    /**
+     * Check Product has URL
+     *
+     * @param \Magento\Catalog\Model\Product $product
+     * @return bool
+     */
+    public function hasProductUrl($product)
+    {
+        if ($product->getVisibleInSiteVisibilities()) {
+            return true;
+        }
+        if ($product->hasUrlDataObject()) {
+            if (in_array($product->hasUrlDataObject()->getVisibility(), $product->getVisibleInSiteVisibilities())) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# AddToCart
+# Xigen Add To Cart Helper
+
+Shortcurt to build add to cart parameters without having worry about template block inheritence
+
+![phpcs](https://github.com/DominicWatts/AddToCart/workflows/phpcs/badge.svg)
+
+![PHPCompatibility](https://github.com/DominicWatts/AddToCart/workflows/PHPCompatibility/badge.svg)
+
+![PHPStan](https://github.com/DominicWatts/AddToCart/workflows/PHPStan/badge.svg)
+
+![php-cs-fixer](https://github.com/DominicWatts/AddToCart/workflows/php-cs-fixer/badge.svg)
+
+## Install instructions
+
+    composer require dominicwatts/addtocart
+
+    php bin/magento setup:upgrade
+
+    php bin/magento setup:di:compile
+
+## Usage instructions
+
+```
+$xigenHelper = $this->helper(Xigen\AddToCart\Helper\AddToCart::class);
+```
+
+```
+$postParams = $xigenHelper->getAddToCartPostParams($_item);
+```
+
+Then pthml as normal e.g.
+
+```
+<form data-role="tocart-form"
+      data-product-sku="<?= $_item->getSku() ?>"
+      action="<?= /* @noEscape */ $postParams['action'] ?>"
+      method="post">
+    <input type="hidden"
+           name="product"
+           value="<?= /* @noEscape */ $postParams['data']['product'] ?>">
+    <?= $block->getBlockHtml('formkey') ?>
+    <input type="number"
+           name="qty"
+           id="qty-<?= $_item->getSku() ?>"
+           maxlength="12"
+           value="1"
+           title="<?php /* @escapeNotVerified */ echo __('Qty') ?>" 
+           class="input-text qty form-control" />
+    <button type="submit"
+            title="<?= /* @noEscape */ __('Add to Basket') ?>"
+            class="action tocart primary btn btn-primary">
+        <span class="d-none d-lg-inline"><?= /* @noEscape */ __('Add to Basket') ?></span>
+        <span class="d-lg-none"><?= /* @noEscape */ __('Add') ?></span>
+    </button>
+</form>
+```
